7b162262649c884e9e6eb89ad5b7f3759b537679
Nicolás Campuzano <nicolas.campuzano@lantri.com>
2019-06-26 12:42:29 -0500

    After presentation

--- test1.py
+++ test1.py
@@ -1,6 +1,5 @@
 tot = 0
 n = 0
-
 def imprimir_explicacion():
 
 	print(str("\nEste programa de ejemplo calcula la suma de los enteros") , str( "\npositivos comprendidos entre 1 y un valor ") , str( "que se ingresa\n"))
